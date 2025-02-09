c4c4812306cdb911ec066a3a9ce6b6a16ce2c459
mandicOfisl <mandicluka62@gmail.com>
2025-02-09 15:50:06 +0100

    prettier formatting

--- src/components/Carousel/animations.ts
+++ src/components/Carousel/animations.ts
@@ -1,64 +1,81 @@
 import { Children } from 'react';
 import CSSTranslate from '../../CSSTranslate';
 import { getPosition, setPosition } from './utils';
-import { AnimationHandler, AnimationHandlerResponse, SwipeAnimationHandler, StopSwipingHandler } from './types';
+import {
+  AnimationHandler,
+  AnimationHandlerResponse,
+  SwipeAnimationHandler,
+  StopSwipingHandler,
+} from './types';
 
 /**
  * Main animation handler for the default 'sliding' style animation
  * @param props
  * @param state
  */
-export const slideAnimationHandler: AnimationHandler = (props, state): AnimationHandlerResponse => {
-    const returnStyles: AnimationHandlerResponse = {};
-    const { selectedItem } = state;
-    const previousItem = selectedItem;
-    const lastPosition = Children.count(props.children) - 1;
-    const needClonedSlide = props.infiniteLoop && (selectedItem < 0 || selectedItem > lastPosition);
+export const slideAnimationHandler: AnimationHandler = (
+  props,
+  state
+): AnimationHandlerResponse => {
+  const returnStyles: AnimationHandlerResponse = {};
+  const { selectedItem } = state;
+  const previousItem = selectedItem;
+  const lastPosition = Children.count(props.children) - 1;
+  const needClonedSlide =
+    props.infiniteLoop && (selectedItem < 0 || selectedItem > lastPosition);
 
-    // Handle list position if it needs a clone
-    if (needClonedSlide) {
-        if (previousItem < 0) {
-            if (props.centerMode && props.centerSlidePercentage && props.axis === 'horizontal') {
-                returnStyles.itemListStyle = setPosition(
-                    -(lastPosition + 2) * props.centerSlidePercentage - (100 - props.centerSlidePercentage) / 2,
-                    props.axis
-                );
-            } else {
-                returnStyles.itemListStyle = setPosition(-(lastPosition + 2) * 100, props.axis);
-            }
-        } else if (previousItem > lastPosition) {
-            returnStyles.itemListStyle = setPosition(0, props.axis);
-        }
-
-        return returnStyles;
-    }
-
-    const currentPosition = getPosition(selectedItem, props);
-
-    // if 3d is available, let's take advantage of the performance of transform
-    const transformProp = CSSTranslate(currentPosition, '%', props.axis);
-
-    const transitionTime = props.transitionTime + 'ms';
-
-    returnStyles.itemListStyle = {
-        WebkitTransform: transformProp,
-        msTransform: transformProp,
-        OTransform: transformProp,
-        transform: transformProp,
-    };
-
-    if (!state.swiping) {
-        returnStyles.itemListStyle = {
-            ...returnStyles.itemListStyle,
-            WebkitTransitionDuration: transitionTime,
-            MozTransitionDuration: transitionTime,
-            OTransitionDuration: transitionTime,
-            transitionDuration: transitionTime,
-            msTransitionDuration: transitionTime,
-        };
+  // Handle list position if it needs a clone
+  if (needClonedSlide) {
+    if (previousItem < 0) {
+      if (
+        props.centerMode &&
+        props.centerSlidePercentage &&
+        props.axis === 'horizontal'
+      ) {
+        returnStyles.itemListStyle = setPosition(
+          -(lastPosition + 2) * props.centerSlidePercentage -
+            (100 - props.centerSlidePercentage) / 2,
+          props.axis
+        );
+      } else {
+        returnStyles.itemListStyle = setPosition(
+          -(lastPosition + 2) * 100,
+          props.axis
+        );
+      }
+    } else if (previousItem > lastPosition) {
+      returnStyles.itemListStyle = setPosition(0, props.axis);
     }
 
     return returnStyles;
+  }
+
+  const currentPosition = getPosition(selectedItem, props);
+
+  // if 3d is available, let's take advantage of the performance of transform
+  const transformProp = CSSTranslate(currentPosition, '%', props.axis);
+
+  const transitionTime = props.transitionTime + 'ms';
+
+  returnStyles.itemListStyle = {
+    WebkitTransform: transformProp,
+    msTransform: transformProp,
+    OTransform: transformProp,
+    transform: transformProp,
+  };
+
+  if (!state.swiping) {
+    returnStyles.itemListStyle = {
+      ...returnStyles.itemListStyle,
+      WebkitTransitionDuration: transitionTime,
+      MozTransitionDuration: transitionTime,
+      OTransitionDuration: transitionTime,
+      transitionDuration: transitionTime,
+      msTransitionDuration: transitionTime,
+    };
+  }
+
+  return returnStyles;
 };
 
 /**
@@ -69,63 +86,70 @@ export const slideAnimationHandler: AnimationHandler = (props, state): Animation
  * @param setState
  */
 export const slideSwipeAnimationHandler: SwipeAnimationHandler = (
-    delta,
-    props,
-    state,
-    setState
+  delta,
+  props,
+  state,
+  setState
 ): AnimationHandlerResponse => {
-    const returnStyles: AnimationHandlerResponse = {};
-    const isHorizontal = props.axis === 'horizontal';
-    const childrenLength = Children.count(props.children);
+  const returnStyles: AnimationHandlerResponse = {};
+  const isHorizontal = props.axis === 'horizontal';
+  const childrenLength = Children.count(props.children);
 
-    const initialBoundry = 0;
+  const initialBoundry = 0;
 
-    const currentPosition = getPosition(state.selectedItem, props);
-    const finalBoundry = props.infiniteLoop
-        ? getPosition(childrenLength - 1, props) - 100
-        : getPosition(childrenLength - 1, props);
+  const currentPosition = getPosition(state.selectedItem, props);
+  const finalBoundry = props.infiniteLoop
+    ? getPosition(childrenLength - 1, props) - 100
+    : getPosition(childrenLength - 1, props);
 
-    const axisDelta = isHorizontal ? delta.x : delta.y;
-    let handledDelta = axisDelta;
+  const axisDelta = isHorizontal ? delta.x : delta.y;
+  let handledDelta = axisDelta;
 
-    // prevent user from swiping left out of boundaries
-    if (currentPosition === initialBoundry && axisDelta > 0) {
-        handledDelta = 0;
+  // prevent user from swiping left out of boundaries
+  if (currentPosition === initialBoundry && axisDelta > 0) {
+    handledDelta = 0;
+  }
+
+  // prevent user from swiping right out of boundaries
+  if (currentPosition === finalBoundry && axisDelta < 0) {
+    handledDelta = 0;
+  }
+
+  let position = currentPosition + 100 / (state.itemSize / handledDelta);
+  const hasMoved = Math.abs(axisDelta) > props.swipeScrollTolerance;
+
+  if (props.infiniteLoop && hasMoved) {
+    // When allowing infinite loop, if we slide left from position 0 we reveal the cloned last slide that appears before it
+    // if we slide even further we need to jump to other side so it can continue - and vice versa for the last slide
+    if (state.selectedItem === 0 && position > -100) {
+      position -= childrenLength * 100;
+    } else if (
+      state.selectedItem === childrenLength - 1 &&
+      position < -childrenLength * 100
+    ) {
+      position += childrenLength * 100;
+    }
+  }
+
+  if (
+    !props.preventMovementUntilSwipeScrollTolerance ||
+    hasMoved ||
+    state.swipeMovementStarted
+  ) {
+    if (!state.swipeMovementStarted) {
+      setState({ swipeMovementStarted: true });
     }
 
-    // prevent user from swiping right out of boundaries
-    if (currentPosition === finalBoundry && axisDelta < 0) {
-        handledDelta = 0;
-    }
+    returnStyles.itemListStyle = setPosition(position, props.axis);
+  }
 
-    let position = currentPosition + 100 / (state.itemSize / handledDelta);
-    const hasMoved = Math.abs(axisDelta) > props.swipeScrollTolerance;
-
-    if (props.infiniteLoop && hasMoved) {
-        // When allowing infinite loop, if we slide left from position 0 we reveal the cloned last slide that appears before it
-        // if we slide even further we need to jump to other side so it can continue - and vice versa for the last slide
-        if (state.selectedItem === 0 && position > -100) {
-            position -= childrenLength * 100;
-        } else if (state.selectedItem === childrenLength - 1 && position < -childrenLength * 100) {
-            position += childrenLength * 100;
-        }
-    }
-
-    if (!props.preventMovementUntilSwipeScrollTolerance || hasMoved || state.swipeMovementStarted) {
-        if (!state.swipeMovementStarted) {
-            setState({ swipeMovementStarted: true });
-        }
-
-        returnStyles.itemListStyle = setPosition(position, props.axis);
-    }
-
-    //allows scroll if the swipe was within the tolerance
-    if (hasMoved && !state.cancelClick) {
-        setState({
-            cancelClick: true,
-        });
-    }
-    return returnStyles;
+  //allows scroll if the swipe was within the tolerance
+  if (hasMoved && !state.cancelClick) {
+    setState({
+      cancelClick: true,
+    });
+  }
+  return returnStyles;
 };
 
 /**
@@ -133,13 +157,16 @@ export const slideSwipeAnimationHandler: SwipeAnimationHandler = (
  * @param props
  * @param state
  */
-export const slideStopSwipingHandler: StopSwipingHandler = (props, state): AnimationHandlerResponse => {
-    const currentPosition = getPosition(state.selectedItem, props);
-    const itemListStyle = setPosition(currentPosition, props.axis);
+export const slideStopSwipingHandler: StopSwipingHandler = (
+  props,
+  state
+): AnimationHandlerResponse => {
+  const currentPosition = getPosition(state.selectedItem, props);
+  const itemListStyle = setPosition(currentPosition, props.axis);
 
-    return {
-        itemListStyle,
-    };
+  return {
+    itemListStyle,
+  };
 };
 
 /**
@@ -147,41 +174,44 @@ export const slideStopSwipingHandler: StopSwipingHandler = (props, state): Anima
  * @param props
  * @param state
  */
-export const fadeAnimationHandler: AnimationHandler = (props, state): AnimationHandlerResponse => {
-    const transitionTime = props.transitionTime + 'ms';
-    const transitionTimingFunction = 'ease-in-out';
+export const fadeAnimationHandler: AnimationHandler = (
+  props,
+  state
+): AnimationHandlerResponse => {
+  const transitionTime = props.transitionTime + 'ms';
+  const transitionTimingFunction = 'ease-in-out';
 
-    let slideStyle: React.CSSProperties = {
-        position: 'absolute',
-        display: 'block',
-        zIndex: -2,
-        minHeight: '100%',
-        opacity: 0,
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0,
-        transitionTimingFunction: transitionTimingFunction,
-        msTransitionTimingFunction: transitionTimingFunction,
-        MozTransitionTimingFunction: transitionTimingFunction,
-        WebkitTransitionTimingFunction: transitionTimingFunction,
-        OTransitionTimingFunction: transitionTimingFunction,
+  let slideStyle: React.CSSProperties = {
+    position: 'absolute',
+    display: 'block',
+    zIndex: -2,
+    minHeight: '100%',
+    opacity: 0,
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    transitionTimingFunction: transitionTimingFunction,
+    msTransitionTimingFunction: transitionTimingFunction,
+    MozTransitionTimingFunction: transitionTimingFunction,
+    WebkitTransitionTimingFunction: transitionTimingFunction,
+    OTransitionTimingFunction: transitionTimingFunction,
+  };
+
+  if (!state.swiping) {
+    slideStyle = {
+      ...slideStyle,
+      WebkitTransitionDuration: transitionTime,
+      MozTransitionDuration: transitionTime,
+      OTransitionDuration: transitionTime,
+      transitionDuration: transitionTime,
+      msTransitionDuration: transitionTime,
     };
+  }
 
-    if (!state.swiping) {
-        slideStyle = {
-            ...slideStyle,
-            WebkitTransitionDuration: transitionTime,
-            MozTransitionDuration: transitionTime,
-            OTransitionDuration: transitionTime,
-            transitionDuration: transitionTime,
-            msTransitionDuration: transitionTime,
-        };
-    }
-
-    return {
-        slideStyle,
-        selectedStyle: { ...slideStyle, opacity: 1, position: 'relative' },
-        prevStyle: { ...slideStyle },
-    };
+  return {
+    slideStyle,
+    selectedStyle: { ...slideStyle, opacity: 1, position: 'relative' },
+    prevStyle: { ...slideStyle },
+  };
 };
--- src/components/Carousel/utils.ts
+++ src/components/Carousel/utils.ts
@@ -4,40 +4,42 @@ import CSSTranslate from '../../CSSTranslate';
 
 export const noop = () => {};
 
-export const defaultStatusFormatter = (current: number, total: number) => `${current} of ${total}`;
+export const defaultStatusFormatter = (current: number, total: number) =>
+  `${current} of ${total}`;
 
-export const isKeyboardEvent = (e?: React.MouseEvent | React.KeyboardEvent): e is React.KeyboardEvent =>
-    e ? e.hasOwnProperty('key') : false;
+export const isKeyboardEvent = (
+  e?: React.MouseEvent | React.KeyboardEvent
+): e is React.KeyboardEvent => (e ? e.hasOwnProperty('key') : false);
 
 /**
  * Gets the list 'position' relative to a current index
  * @param index
  */
 export const getPosition = (index: number, props: CarouselProps): number => {
-    if (props.infiniteLoop) {
-        // index has to be added by 1 because of the first cloned slide
-        ++index;
+  if (props.infiniteLoop) {
+    // index has to be added by 1 because of the first cloned slide
+    ++index;
+  }
+
+  if (index === 0) {
+    return 0;
+  }
+
+  const childrenLength = Children.count(props.children);
+  if (props.centerMode && props.axis === 'horizontal') {
+    let currentPosition = -index * props.centerSlidePercentage;
+    const lastPosition = childrenLength - 1;
+
+    if (index && (index !== lastPosition || props.infiniteLoop)) {
+      currentPosition += (100 - props.centerSlidePercentage) / 2;
+    } else if (index === lastPosition) {
+      currentPosition += 100 - props.centerSlidePercentage;
     }
 
-    if (index === 0) {
-        return 0;
-    }
+    return currentPosition;
+  }
 
-    const childrenLength = Children.count(props.children);
-    if (props.centerMode && props.axis === 'horizontal') {
-        let currentPosition = -index * props.centerSlidePercentage;
-        const lastPosition = childrenLength - 1;
-
-        if (index && (index !== lastPosition || props.infiniteLoop)) {
-            currentPosition += (100 - props.centerSlidePercentage) / 2;
-        } else if (index === lastPosition) {
-            currentPosition += 100 - props.centerSlidePercentage;
-        }
-
-        return currentPosition;
-    }
-
-    return -index * 100;
+  return -index * 100;
 };
 
 /**
@@ -45,12 +47,22 @@ export const getPosition = (index: number, props: CarouselProps): number => {
  * @param position
  * @param forceReflow
  */
-export const setPosition = (position: number, axis: 'horizontal' | 'vertical'): React.CSSProperties => {
-    const style = {};
-    ['WebkitTransform', 'MozTransform', 'MsTransform', 'OTransform', 'transform', 'msTransform'].forEach((prop) => {
-        // @ts-ignore
-        style[prop] = CSSTranslate(position, '%', axis);
-    });
+export const setPosition = (
+  position: number,
+  axis: 'horizontal' | 'vertical'
+): React.CSSProperties => {
+  const style = {};
+  [
+    'WebkitTransform',
+    'MozTransform',
+    'MsTransform',
+    'OTransform',
+    'transform',
+    'msTransform',
+  ].forEach((prop) => {
+    // @ts-ignore
+    style[prop] = CSSTranslate(position, '%', axis);
+  });
 
-    return style;
+  return style;
 };
